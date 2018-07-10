0e11a55ea4bfcfd9501d3e97868ee90617e2d0f8
Seung Eun (Sophia) Lee <lsemd96@gmail.com>
2018-07-10 17:02:30 +0900

    Update assignment3_1.cpp

--- assignment3_1.cpp
+++ assignment3_1.cpp
@@ -1,7 +1,8 @@
 // Seung Eun Lee
 // 2018-07-02
 // Coins
-//
+//Write a program that prompts the user to input a number of quarters, dimes, and nickels. 
+//The program then outputs the total value of the coins in pennies.
 #include <iostream>
 using namespace std;
 
