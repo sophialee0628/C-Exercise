3502d41a3fa5ccf295051ad8aea6e0c3826f46f6
Seung Eun (Sophia) Lee <lsemd96@gmail.com>
2018-07-10 17:07:21 +0900

    Update assignment5_2.cpp

--- assignment5_2.cpp
+++ assignment5_2.cpp
@@ -1,6 +1,16 @@
 // Seung Eun Lee
 // 2018-07-05
 // Number guessing game
+/**
+A new author is in the process of negotiating a contract for a new romance novel. The publisher is offering three options. 
+In the first option, the author is paid $5,000 upon delivery of the final manuscript and $20,000 when the novel is published. 
+In the second option, the author is paid 12.5% of the net price of the novel for each copy of the novel sold. 
+In the third option, the author is paid 10% of the net price for the first 4000 copies sold, and 14% of the net price for the copies sold over 4000. 
+The author has some idea about the number of copies that will be sold and would like to have an estimate of the royalties generated under each option. 
+Write a program that prompts the author to enter the net price of each copy of the novel and the estimated number of copies that will be sold. 
+The program then outputs the royalties under each option and the best option the author could choose. 
+(Use appropriate named constants to store the special values such as royalties rates and fixed royalties.)
+**/
 
 #include <iostream>
 #include <cmath>
