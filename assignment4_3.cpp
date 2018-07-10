c00f7bafa3931a6caa78c9878fad9ae21663f932
Seung Eun (Sophia) Lee <lsemd96@gmail.com>
2018-07-10 17:05:56 +0900

    Update assignment4_3.cpp

--- assignment4_3.cpp
+++ assignment4_3.cpp
@@ -1,6 +1,17 @@
 // Seung Eun Lee
 // 2018-07-03
 // solutions of equation using cmath
+/**The roots of the quadratic equation ax2 + bx + c = 0, a ≠ 0 are given by
+the following formula:
+In this formula, the term b^2-4ac is called the discriminant.
+If b^2-4ac = 0 , then the equation has a single (repeated) root.
+If b^2-4ac > 0, the equation has two real roots.
+If b^2-4ac < 0, the equation has two complex roots.
+Write a program that prompts the user to input the value of a (the coefficient of x2), b (the coefficient of x), and c (the constant term) and outputs the type of roots of the equation. Furthermore, if b^2-4ac>0, theprogram should output the roots of the quadratic equation. (Hint: Use
+the function pow from the header file cmath to calculate the square
+root.) **/
+
+
 #include <iostream>
 #include <cmath>
 
