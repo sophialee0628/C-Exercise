253476f6b665b3aa0617e594263d12a09e5934ca
Seung Eun (Sophia) Lee <lsemd96@gmail.com>
2018-07-10 17:04:29 +0900

    Update assignment4_2.cpp

--- assignment4_2.cpp
+++ assignment4_2.cpp
@@ -1,6 +1,13 @@
 // Seung Eun Lee
 // 2018-07-03
 // Pythagorean theorem
+/**In a right triangle, the square of the length of one
+side is equal to the sum of the squares of the
+lengths of the other two sides. Write a program
+that prompts the user to enter the lengths of three
+sides of a triangle and then outputs a message
+indicating whether the triangle is a right triangle.**/
+
 #include <iostream>
 
 using namespace std;
